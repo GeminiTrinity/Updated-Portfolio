dff09c570e9d2d62d40ec2205b296107583f8f2a
Jacob Krueger <jmk4u14@gmail.com>
2020-11-14 04:27:57 -0500

    Modified video code. Added functionality to only display video and then remove it and display site after 7 sec

---
+++ script.js
@@ -0,0 +1,6 @@
+var getSite = function (){
+    setTimeout(function(){	
+        document.getElementById("entrance").style.display="none";
+        document.getElementById("website").style.display="block"},
+        7000);
+};
